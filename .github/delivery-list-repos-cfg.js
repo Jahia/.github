aafa99011ad3848eef501306457a4ca5c15f8682
Francois G. <fgerthoffert@jahia.com>
2025-01-06 06:03:15 -0500

    feat: Added a workflow to list product repositories (#45)

---
+++ .github/delivery-list-repos-cfg.js
@@ -0,0 +1,12 @@
+// This file contains the json2csv transformation config to create a CSV
+// See: https://juanjodiaz.github.io/json2csv/#/parsers/cli
+export default {
+    fields: [
+      {
+        label: '',
+        value: row => {
+          return row.nameWithOwner + '@' + row.defaultBranchRef.name
+        }
+      }
+    ]
+  }
